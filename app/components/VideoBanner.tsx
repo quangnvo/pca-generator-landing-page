2612c89c887d7f70fbe1ed92d5de10839a0467bf
quangnvo <voquang.usth@gmail.com>
2024-05-19 10:17:12 +0800

    update videobanner

--- app/components/VideoBanner.tsx
+++ app/components/VideoBanner.tsx
@@ -1,42 +1,20 @@
-"use client"
-
-import ReactPlayer from 'react-player';
-import Video from 'next-video';
 import Image from 'next/image';
 
-
-
 const VideoBanner = () => {
-
-
     return (
-        // <div className="w-full md:w-9/12 mx-auto border border-gray-300 rounded-md shadow-xl overflow-hidden">
-        //     <Video
-        //         src="/videos/aaaaa.mp4"
-        //     />
-        // </div>
-        // <div className="w-full md:w-9/12 mx-auto border border-gray-300 rounded-md shadow-xl overflow-hidden">
-        //     <ReactPlayer
-        //         width="100%"
-        //         height="100%"
-        //         url="/videos/aaaaa.mp4"
-        //         controls={true}
-        //         loop={true}
-        //         volume={0}
-        //         muted={true}
-        //     />
-        // </div>
-
-
-        <Image
-            width="800"
-            height="450"
-            src="/images/pca-generator-screenshot.gif"
-            alt="pca-generator-screenshot"
-            unoptimized={true}
-            quality={100}
-        />
-
+        <div className="flex flex-col gap-10">
+            <h2 className="text-3xl md:text-4xl font-semibold leading-relaxed text-center">Demo</h2>
+            <div className="h-[55vh] md:h-[75vh] w-full md:w-9/12 mx-auto overflow-hidden border border-gray-300 rounded-md ">
+                <Image
+                    width="800"
+                    height="450"
+                    src="/images/123123.gif"
+                    alt="pca-generator-screenshot"
+                    unoptimized={true}
+                    quality={100}
+                />
+            </div>
+        </div>
     )
 }
 
