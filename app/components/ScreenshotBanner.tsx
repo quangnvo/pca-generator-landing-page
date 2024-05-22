80946f07c98f328f674cd93c58179221953ee381
quangnvo <voquang.usth@gmail.com>
2024-05-22 14:08:21 +0800

    update height of screenshot

--- app/components/ScreenshotBanner.tsx
+++ app/components/ScreenshotBanner.tsx
@@ -1,7 +1,7 @@
 
 const ScreenshotBanner = () => {
     return (
-        <div className="relative h-[55vh] md:h-[75vh] w-full md:w-9/12 mx-auto overflow-hidden border border-gray-300 rounded-md shadow-xl image-scroll-from-top-to-bottom">
+        <div className="relative h-[45vh] md:h-[75vh] w-full md:w-9/12 mx-auto overflow-hidden border border-gray-300 rounded-md shadow-xl image-scroll-from-top-to-bottom">
             <img
                 src="/images/pca-generator-screenshot.png"
                 alt="pca-generator-screenshot"
