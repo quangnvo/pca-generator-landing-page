9858388af103a4ed920e09cedcc9ef5769672709
quangnvo <voquang.usth@gmail.com>
2024-05-24 10:58:54 +0800

    update built with

--- app/components/BuiltWith.tsx
+++ app/components/BuiltWith.tsx
@@ -28,6 +28,10 @@ function BuiltWith() {
                         </blockquote>
                     </figure>
                 </Marquee>
+
+                <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3 bg-gradient-to-r from-white dark:from-background"></div>
+
+                <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
             </div>
         </div>
     )
